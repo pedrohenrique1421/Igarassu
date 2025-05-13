416ebd1657f13a7cdf4228e39e7fb6cc0b19387b
PEDRO HENRIQUE DE ANDRADE LINS <pedro.hlins@a.ficr.edu.br>
2025-05-13 10:37:20 -0300

    Agendamento

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import styles from "./styles.module.css";
 import { NavLink } from "react-router-dom";
 import { HandleSetFormType } from "../../manager"; // Os tipos de form estao aqui
+import { useNavigate } from "react-router-dom";
 
 import Img from "../../assets/logo igarassu.svg";
+import Img2 from "../../assets/BAckArrow.svg";
 
 export default function Header({ type }) {
+    const navigate = useNavigate();
     return (
         <div className={styles.Container}>
             <img src={Img} alt="" />
@@ -19,7 +22,14 @@ export default function Header({ type }) {
                     </button>
                 </NavLink>
             ) : (
-                <></>
+                <button
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >
+                    <img src={Img2} alt="" />
+                    <h6>Voltar</h6>
+                </button>
             )}
         </div>
     );
