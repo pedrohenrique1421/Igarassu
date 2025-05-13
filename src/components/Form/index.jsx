cec4cd0044056c8c8c0990a6e1125aa65e4c8e43
PEDRO HENRIQUE DE ANDRADE LINS <pedro.hlins@a.ficr.edu.br>
2025-05-13 10:24:49 -0300

    FAzendos os tipos dos forms

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,43 +1,85 @@
+import styles from "./styles.module.css";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function Form({ type }) {
-    const navigate = useNavigate();
-
     switch (type) {
         case "agendamento":
             return <Agendamento />;
         default:
-            const [count, setCount] = useState(10);
-
-            useEffect(() => {
-                const interval = setInterval(() => {
-                    setCount((prev) => {
-                        if (prev <= 0) {
-                            clearInterval(interval); // Para o intervalo
-                            navigate("/"); // Redireciona quando o contador atinge 0
-                            return prev; // Evita atualização adicional do estado
-                        }
-                        return prev - 1; // Atualiza o estado
-                    });
-                }, 1000);
-
-                // Limpa o intervalo quando o componente é desmontado
-                return () => clearInterval(interval);
-            }, [navigate]);
-
-            return (
-                <div>
-                    <h1>Error 404</h1>
-                    <h6>
-                        Redirecionando para tela inicial em {count.toFixed(1)}{" "}
-                        segundos
-                    </h6>
-                </div>
-            );
+            return <ErrorPage />;
     }
 }
 
+//#region Agendamento
 function Agendamento() {
-    return <h1>Olá Agendamento</h1>;
+    const [ajuda, setAjuda] = useState(false);
+    return (
+        <div className={styles.Container}>
+            {!ajuda ? (
+                <div className={styles.content}>
+                    <h1>Agendamento de visita</h1>
+                    <div className={styles.line} />
+                    <input type="text" placeholder="Situação" />
+                    <input type="text" placeholder="Descrição | Endereço" />
+                    <input type="submit" value="Fazer Agendamento" />
+                </div>
+            ) : (
+                <div className={styles.content}>
+                    <h1>Agendamento de visita</h1>
+                    <div className={styles.line} />
+                    <p>
+                        Agende uma visita do assistente social à sua residência
+                        para fazer a solicitação de um benefício
+                    </p>
+                </div>
+            )}
+            <h2
+                onClick={() => {
+                    setAjuda(!ajuda);
+                }}
+            >
+                Precisa de ajuda?
+            </h2>
+        </div>
+    );
 }
+
+//#endregion
+
+//#region Error
+function ErrorPage() {
+    const [count, setCount] = useState(10);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((prev) => {
+                if (prev <= 0) {
+                    clearInterval(interval); // Para o intervalo
+                    navigate("/"); // Redireciona quando o contador atinge 0
+                    return prev; // Evita atualização adicional do estado
+                }
+                return prev - 1; // Atualiza o estado
+            });
+        }, 1000);
+
+        // Limpa o intervalo quando o componente é desmontado
+        return () => clearInterval(interval);
+    }, [navigate]);
+
+    return (
+        <div className={styles.Container}>
+            <div className={styles.content}>
+                <h1>Error 404</h1>
+                <div className={styles.line} />
+                <h4>
+                    Redirecionando para tela inicial em {count.toFixed(1)}{" "}
+                    segundos
+                </h4>
+            </div>
+        </div>
+    );
+}
+
+//#endregion
