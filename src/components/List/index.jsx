7aa1798a640fcc2e7d221f73d705ae222d849e74
PEDRO HENRIQUE DE ANDRADE LINS <pedro.hlins@a.ficr.edu.br>
2025-05-23 10:33:31 -0300

    dinamização da lista de beneficiarios

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -21,17 +21,34 @@ export default function List({ selected, admPermission }) {
     }
 }
 
+import img from "../../assets/Search.svg";
+import dataBene from "../../manager/exem.js";
+import { useState } from "react";
+
 function Solicitantes() {
+    const [bene, setBene] = useState([]);
+
+    const RenderizarLista = () => {};
     return (
         <div className={styles.Container}>
-            <input
-                type="text"
-                name="SeachBar"
-                placeholder="Pesquisar por NIS"
-            />
+            <div className={styles.searchBar}>
+                <input
+                    type="text"
+                    name="SeachBar"
+                    placeholder="Pesquisar por NIS..."
+                />
+                <img src={img} className={styles.search} alt="" />
+            </div>
             <div className={styles.lista}>
-                <div className={styles.itemLista}>
-                    <p>Ronaldo</p>
+                <div className={styles.header}>
+                    <h3>Nome</h3>
+                    <h3>CPF</h3>
+                    <h3>Email</h3>
+                    <h3>Idade</h3>
+                    <h3>Telefone</h3>
+                </div>
+                <div className={styles.itemsLista}>
+                    {/* Aqui entra os componentes dinamiocos */}
                 </div>
             </div>
         </div>
